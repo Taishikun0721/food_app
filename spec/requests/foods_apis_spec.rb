3df7b1ac9d964191485cbeb52ea6c36f6a80e9e4
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-09-16 00:59:01 +0900

    テストを修正 #17

--- spec/requests/foods_apis_spec.rb
+++ spec/requests/foods_apis_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "FoodsApis", type: :request do
 
   let(:user_a) { FactoryBot.create(:user) }
   let(:category_a) { FactoryBot.create(:category) }
-  let(:food_a) { FactoryBot.create(:food) }
+  let(:food_a) { FactoryBot.build(:food) }
 
   describe "GET" do
     it 'works to foods_path' do
@@ -16,10 +16,10 @@ RSpec.describe "FoodsApis", type: :request do
       get new_food_path
       expect(response).to have_http_status '200'
     end
+
+    it 'works to food_path' do
+      get foods_path(food_a)
+      expect(response).to have_http_status '200'
+    end
   end
-
-  describe 'POST' do
-
-  end
-
 end
