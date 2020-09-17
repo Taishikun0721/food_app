416599f5fcf4d036c6bea6f2c36098a8b1bc7948
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-09-17 23:17:45 +0900

    プロフィール機能を実装 #19

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,29 @@
 class ProfilesController < ApplicationController
-  def show; end
+  before_action :set_user, only: [:edit, :update]
+  def show
 
-  def edit; end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(profile_params)
+      reset_session
+      redirect_to login_path, notice: 'ユーザー情報を更新しました。ログインし直して下さい。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find_by(id: current_user.id)
+  end
 end
