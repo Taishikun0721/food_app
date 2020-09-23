e44dd91378edf2a74dd1fe590adf9cfea084b8fc
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-09-23 09:15:59 +0900

    テストを修正 #35

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -5,15 +5,24 @@ RSpec.describe Food, type: :model do
   let(:category_a) { FactoryBot.create(:category) }
   let(:food_a) { FactoryBot.build(:food, user: user_a, category: category_a) }
 
-  describe 'must exists' do
+  describe 'validation test' do
 
-    before do
-      food_a.image = fixture_file_upload('spec/factories/images/after.png')
+    context 'with image' do
+      before do
+        food_a.image = fixture_file_upload('spec/factories/images/after.png')
+      end
+
+      it 'be able to make valid data' do
+        food_a.valid?
+        expect(food_a).to be_valid
+      end
     end
 
-    it 'valid data' do
-      food_a.valid?
-      expect(food_a).to be_valid
+    context 'without image' do
+      it 'invalid without image' do
+        food_a.tap(&:save).valid?
+        expect(food_a.errors[:image]).to include '画像を添付して下さい。'
+      end
     end
   end
 end
