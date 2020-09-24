f965f4c19a604c11705d87270344bf0b8ac251ed
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-09-24 23:41:41 +0900

    バリデーションを追加 #45

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,10 +6,22 @@ class Food < ApplicationRecord
 
   validates :description, presence: true, length: { maximum: 100 }
   validate :image_required
+  validate :unpermitted_extension
 
   private
 
   def image_required
     errors.add(:image, '画像を添付して下さい。') unless image.attached?
+  end
+
+  def unpermitted_extension
+    if image.attached? && !image?
+      image.purge
+      errors.add(:image, '画像以外は添付できません。')
+    end
+  end
+
+  def image?
+    %w[image/jpg image/jpeg image/gif image/png].include?(image.blob.content_type) if image.attached?
   end
 end
