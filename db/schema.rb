d9f8aee9bd6fe0fa9bf9646003403039050e5d24
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-09-27 00:09:18 +0900

    UNIQUE制約とNOTNULL制約を追加 #48

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_145525) do
+ActiveRecord::Schema.define(version: 2020_09_26_150603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_145525) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -53,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_145525) do
   end
 
   create_table "foods", force: :cascade do |t|
-    t.text "description"
+    t.text "description", null: false
     t.bigint "user_id"
     t.bigint "category_id"
     t.datetime "created_at", null: false
@@ -69,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_145525) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
